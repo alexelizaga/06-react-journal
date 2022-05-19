e18504042dee5c0b5c1e953fa4a37a49fd8431c1
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-19 23:29:52 +0200

    249

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+
 import { authReducer } from '../reducers/authReducer';
 
 const reducer = combineReducers({
     auth: authReducer
 })
 
-export const store = configureStore({reducer});
+export const store = configureStore({
+    reducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            thunk: {
+                // extraArgument: myCustomApiService
+            }
+        })
+});
