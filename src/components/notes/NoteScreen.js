ce0ba5bc6254680dac4e8accc59f78c340c64dfb
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-19 18:35:28 +0200

    239

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -4,6 +4,29 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
         <NotesAppBar />
+
+        <div className="notes__content">
+
+            <input
+                type="text"
+                placeholder="Some awesome title"
+                className="notes__title-input"
+            />
+
+            <textarea
+                placeholder="what happened today"
+                className="notes__text-area"
+                autoComplete="off"
+            ></textarea>
+
+            <div className="notes__image">
+                <img
+                    src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                    alt="image"
+                />
+            </div>
+
+        </div>
     </div>
   )
 }
