7da11054ca20c0cf38db33c7f143cf5e40d8122d
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-19 23:09:41 +0200

    248

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import { authReducer } from '../reducers/authReducer';
 
 const reducer = combineReducers({
     auth: authReducer
 })
 
-export const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export const store = configureStore({reducer});
