34451b1ab9c096781d06dbb6b358c5fdb16f310c
alexelizaga <alexelizaga@icloud.com>
2022-05-19 11:45:28 +0200

    232

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 export const LoginScreen = () => {
   return (
@@ -27,25 +27,24 @@ export const LoginScreen = () => {
         >
           Login
         </button>
-        
+
         <div className="auth__social-networks">
-          <p>Login with social networks</p>
-
-          <div 
-              className="google-btn"
-          >
-              <div className="google-icon-wrapper">
-                  <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
-              </div>
-              <p className="btn-text">
-                  <b>Sign in with google</b>
-              </p>
+          <div className="google-btn" >
+            <div className="google-icon-wrapper">
+                <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+            </div>
+            <p className="btn-text">
+                <b>Sign in with google</b>
+            </p>
           </div>
-
-          <Link to='/auth/register'>
-            Create new account
-          </Link>
         </div>
+
+        <Link
+          to='/auth/register'
+          className="link"
+        >
+          Create new account
+        </Link>
       </form>
     </>
   )
