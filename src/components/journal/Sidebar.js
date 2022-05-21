dd8c5d5091df2627c24e888df2fce68863cfa284
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-21 12:19:08 +0200

    267

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,13 +1,13 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { startLogout } from '../../actions/auth';
 
 import { JournalEntries } from "./JournalEntries";
 
 export const Sidebar = () => {
   const dispatch = useDispatch();
+  const { name } = useSelector( state => state.auth );
 
   const handleLogout = () => {
-    console.log('click');
     dispatch( startLogout() );
   };
 
@@ -16,8 +16,8 @@ export const Sidebar = () => {
 
       <div className="journal__sidebar-navbar">
         <h3 className="mt-5">
-          <i className="far fa-moon"></i>
-          <span> Alex</span>
+          <i class="fa-solid fa-user"></i>
+          <span> {name}</span>
         </h3>
 
         <button
