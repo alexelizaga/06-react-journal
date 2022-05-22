3550a33fa5d3046784d564f82884d4b1bb6f1b0f
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-22 11:48:00 +0200

    272

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -15,8 +15,7 @@ import { PrivateRoute } from './PrivateRoute';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { login } from '../actions/auth';
 import { PublicRoute } from './PublicRoute';
-import { loadNotes } from '../helpers/loadNotes';
-import { setNotes } from '../actions/notes';
+import { startLoadingNotes } from '../actions/notes';
 
 export const AppRouter = () => {
 
@@ -32,8 +31,7 @@ export const AppRouter = () => {
         dispatch( login(user.uid, user.displayName) );
         setIsLoggedIn(true);
 
-        const notes = await loadNotes(user.uid);
-        dispatch( setNotes( notes ) );
+        dispatch( startLoadingNotes( user.uid ) );
       } else {
         setIsLoggedIn(false);
       }
