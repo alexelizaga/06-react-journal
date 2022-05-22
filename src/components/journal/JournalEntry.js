1ee41120232aad82775d90e50f4bf2c000beff07
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-22 12:44:15 +0200

    274

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,10 +1,26 @@
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
+import { activeNote } from '../../actions/notes';
 
 export const JournalEntry = ({ id, date, title, body, url }) => {
   const noteDate = moment(date);
+  const dispatch = useDispatch();
+
+  const handleEntryClick = () => {
+    console.log('EntryClick');
+    dispatch (
+      activeNote(id, {
+        date, title, body, url
+      })
+    );
+  }
 
   return (
-    <div className="journal__entry pointer">
+    <div
+      className="journal__entry pointer"
+      onClick={ handleEntryClick }
+    >
       
       {
         url && (
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,32 +1,51 @@
-import { NotesAppBar } from "./NotesAppBar"
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+
+import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
-  return (
-    <div className="notes__main-content">
-        <NotesAppBar />
 
-        <div className="notes__content">
+    const { active:note } = useSelector(state => state.notes);
+    const [ formValues, handleInputChange ] = useForm(note);
 
-            <input
-                type="text"
-                placeholder="Some awesome title"
-                className="notes__title-input"
-            />
+    console.log( formValues );
+    const { id, date, title, body } = formValues;
 
-            <textarea
-                placeholder="what happened today"
-                className="notes__text-area"
-                autoComplete="off"
-            ></textarea>
+    return (
+        <div className="notes__main-content">
+            <NotesAppBar />
 
-            <div className="notes__image">
-                <img
-                    src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                    alt="imagen"
+            <div className="notes__content">
+
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    value={ title }
+                    onChange={ handleInputChange }
                 />
-            </div>
 
+                <textarea
+                    placeholder="what happened today"
+                    className="notes__text-area"
+                    autoComplete="off"
+                    value={ body }
+                    onChange={ handleInputChange }
+                ></textarea>
+
+                {
+                    note.url &&
+                    (
+                        <div className="notes__image">
+                            <img
+                                src={ note.url }
+                                alt="imagen"
+                            />
+                        </div>
+                    )
+                }
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
